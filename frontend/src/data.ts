1903088582fc745e24da6aefac633b195c199e6b
Americano.Dev <andrenations@gmail.com>
2024-08-14 22:35:48 -0400

    Add data for product information including headers, images, and descriptions

---
+++ frontend/src/data.ts
@@ -0,0 +1,21 @@
+// src/data.ts
+const data = [
+    {
+      header: 'Pest control innovation',
+      image: 'images/p1.webp',
+      text: 'LED lamps for unrivaled fly control and protection in high-dependency environments such as Food Processing, Pharmaceutical',
+    },
+    {
+      header: 'Smart Thermostat Enhanced',
+      image: 'images/p3.webp',
+      text: 'ecobee New Smart Thermostat Enhanced - Programmable Wifi Thermostat - Works with Siri, Alexa, Google Assistant - Energy Star Certified - Smart Home.',
+    },
+    {
+      header: 'Video System With LED Lights',
+      image: 'images/p2.png',
+      text: 'UWS-3510/D Complete Dual Color HIGH DEFINITION Video System With LED Lights - SMP Ltd',
+    },
+  ];
+  
+  export default data;
+  
